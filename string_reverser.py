54cc4a4c794e9540042efae08c3cf6c09d709510
John Doe <johndoe@email.com>
2024-12-14 01:05:09 +0530

    rm

--- string_reverser.py
+++
@@ -1,7 +0,0 @@
-def reverse_string(s):
-    return s[::-1]
-
-original_string = "Hello World"
-reversed_string = reverse_string(original_string)
-print("Original String:", original_string)
-print("Reversed String:", reversed_string)
